9d46e08d21ae5d6798e34e03c6e879ee4e1f8ad6
Trevor Cash <trevorcash@MacBook-Pro.local>
2021-06-20 00:37:15 -0400

    - Added router and switches to determine what page is being viewed
    - Added file structure and place holder for the sites static pages verify router is working correctly.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+
+//Import Components
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
+
+//Import Pages
+import Home from './Pages/Home/Home'
+import Contact from './Pages/Contact/Contact'
+import Info from './Pages/Info/Info'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Router>
+        <Switch>
+          {/* Followed Alex's structure to potentiall add pages with no header or footer */}
+          <Route path="*">
+            <Header />
+            <div className="Container">
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route exact path="/contact">
+                  <Contact />
+                </Route>
+                <Route exact path="/info">
+                  <Info />
+                </Route>
+              </Switch>
+            </div>
+            <Footer />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/Info/Info.js
@@ -0,0 +1,11 @@
+import './Info.css';
+
+function Info() {
+  return (
+    <div className="Info">
+        Info
+    </div>
+  );
+}
+
+export default Info;
