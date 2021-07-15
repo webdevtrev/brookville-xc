f73ac7b081f54717c7e34c473bbd1fad303cd44f
Trevor Cash <trevorcash@MacBook-Pro.local>
2021-07-15 19:09:55 -0400

    Added links and copyright to footer with quote pulling from sanity

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,29 +1,67 @@
 import { Link } from "react-router-dom";
 import './Footer.scss';
+import React, { useEffect, useState } from "react";
+import sanityClient from '../../client';
+
+import { FaStrava, FaTwitter, FaPhone } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
 
 function Footer() {
+
+  const [footer, setData] = useState(null);
+  const [socials, setSocials] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "footer"]`
+      )
+      .then((data) => {
+        setData(data[0])
+      })
+      .catch(console.error);
+
+    sanityClient
+      .fetch(
+        `*[_type == "socialLinks"]`
+      )
+      .then((data) => {
+        setSocials(data[0])
+      })
+      .catch(console.error);
+  }, []);
+
+  console.log(footer)
+  console.log(socials)
   return (
     <div className="Footer">
+    <div className="Top">
 
-<div class="philosophy">
-                <h2>Program Philosophy:</h2>
-                <p>Everyone is important in Cross Country!  Nobody sits the bench.  Every runner is different in natural ability and talent. Regardless of your starting talent, everyone can become better with consistent intelligent training.</p>
-                <p>The skills learned in cross country can be used by athletes to become the following:</p>
-                <ol>
-                    <li>Better students</li>
-                    <li>Better runners  </li>
-                    <li>Better teammates </li>
-                    <li>Productive adults</li>
-                </ol>
-            </div>
-            <div className="links">
-  <Link to='/'>Home</Link>
-          <Link to='/practiceInfo'>practiceInfo</Link>
-          <Link to='/faq'>FAQ</Link>
-          <Link to='/contact'>Contact</Link>
-          <Link to='/licenses'>Licenses</Link>
-            </div>
-        
+      <div class="philosophy">
+        <h2>Program Philosophy:</h2>
+        <p>{footer.quote}</p>
+      </div>
+      <div className="links">
+        <Link to='/'>Home</Link>
+        <Link to='/practiceInfo'>practiceInfo</Link>
+        <Link to='/faq'>FAQ</Link>
+        <Link to='/contact'>Contact</Link>
+      </div>
+      
+    </div>
+    <div className="Bottom">
+      <div className="buttons">
+        {footer?.phone && <a href={"tel:" + socials?.phone}><FaPhone /></a>}
+        {footer?.email && <a href={"mailto:" + socials?.email}><MdEmail /></a>}
+        {footer?.twitter && <a href={socials?.twitter}><FaTwitter /></a>}
+        {footer?.strava && <a href={socials?.strava}><FaStrava /></a>}
+      </div>
+      <div className="copyright">Copyright © 2021 Brookville XC, All rights reserved</div>
+      <div>
+        <Link to='/licenses'>Licenses</Link>
+      </div>
+
+    </div>
     </div>
   );
 }
